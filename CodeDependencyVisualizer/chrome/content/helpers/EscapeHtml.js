c4aa4bd87cec43a04ce9bbf808f772b2a75aba0c
kgmsoft <princehome0505@gmail.com>
2012-05-10 14:22:59 +0200

    Added comments in EscapeHtml

--- CodeDependencyVisualizer/chrome/content/helpers/EscapeHtml.js
+++ CodeDependencyVisualizer/chrome/content/helpers/EscapeHtml.js
@@ -1,5 +1,10 @@
 encoder =
 {
+    /**     Enables escaping <br> and other HTML elements represented in strings
+     *      An example if HTML in a string isn't escaped: te<br>st
+     *      Shows as: te
+     *                st
+      */
     escapeHtml: function(str)
     {
         var div = document.createElement('div');
